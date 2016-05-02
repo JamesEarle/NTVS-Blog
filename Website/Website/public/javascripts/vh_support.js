d17544ec4d8dd8ba137efb823477daf7548a9b57
James Earle <j_earle@hotmail.com>
2016-05-02 17:52:06 -0400

    scroll.js and vh unit support via jQuery

---
+++ Website/Website/public/javascripts/vh_support.js
@@ -0,0 +1,24 @@
+﻿/*
+    Additional support for use of the 'vh' unit in CSS. 
+*/
+
+$(document).ready(function () {
+    var height = $(window).height();
+
+    var cssSelectors = [
+        ".overlay",
+        ".overlay-half-down",
+        ".overlay-half-up",
+        ".headline-background",
+        ".headline-background-sm"
+    ];
+
+    for (var key in cssSelectors) {
+        if (cssSelectors[key] === ".headline-background-sm") {
+            $(cssSelectors[key]).css("height", 2 * height);
+        } else {
+            $(cssSelectors[key]).css("height", height);
+        }
+    }
+
+});
